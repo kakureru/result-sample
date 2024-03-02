8b9749e3940bf0ac8a898736673f73e0ff856e81
svyatoslav lukyanov <sviatoslav-lv@yandex.ru>
2024-03-02 17:37:44 +0300

    Add flatMap operator

--- common/core/src/main/java/com/resultsample/common/core/Operators.kt
+++ common/core/src/main/java/com/resultsample/common/core/Operators.kt
@@ -16,3 +16,11 @@ inline fun <T> ResultState<T>.onError(action: (Exception, data: T?) -> Unit): Re
     if (this is ResultState.Error) action(error, data)
     return this
 }
+
+inline fun <R, T> ResultState<T>.flatMap(transform: (value: T) -> ResultState<R>): ResultState<R> {
+    return when(this) {
+        is ResultState.Success -> transform(data)
+        is ResultState.Error -> ResultState.Error(error, data?.let(transform)?.data)
+        is ResultState.Loading -> ResultState.Loading(data?.let(transform)?.data)
+    }
+}
